dfc55302a67d60d7a0fd30919f5f5cbc32e921a6
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2026-07-01 23:21:30 -0400

    entity, repository y service de MS cursos

---
+++ msvc-cursos/src/main/java/org/mjdiazc/springcloud/msvc/cursos/repositories/CursoRepository.java
@@ -0,0 +1,7 @@
+package org.mjdiazc.springcloud.msvc.cursos.repositories;
+
+import org.mjdiazc.springcloud.msvc.cursos.entity.Curso;
+import org.springframework.data.repository.CrudRepository;
+
+public class CursoRepository extends CrudRepository<Curso, Long> {
+}
