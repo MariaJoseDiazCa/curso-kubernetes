e4e0faeff5d02f4b889db1adff600bbd9ef8c9e9
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2026-07-15 10:57:58 -0400

    cambios ms curso

--- msvc-usuarios/src/main/java/org/mjdiazc/springcloud/msvc/usuarios/repositories/UsuarioRepository.java
+++ msvc-usuarios/src/main/java/org/mjdiazc/springcloud/msvc/usuarios/repositories/UsuarioRepository.java
@@ -4,4 +4,8 @@ import org.mjdiazc.springcloud.msvc.usuarios.models.entity.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsuarioRepository  extends CrudRepository<Usuario, Long> {
+
+
 }
+
+
